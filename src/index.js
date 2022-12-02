1702955adec546c14e0c775c7ab5ac3791ec1a09
Inna <isko.inna.94@gmail.com>
2022-12-02 23:15:22 +0200

    1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import firebase from 'firebase/firebase-node';
 import pagination from 'pagination';
 
 const getRef = selector => document.querySelector(selector);
@@ -22,7 +23,7 @@ function createMarkup(properties) {
   return `
      <li class="movies__item" data-id="${properties.id}">
      <a class="movies__link">
-        <img class="movies__img" src="${properties.backdrop_path}" alt="" />
+        <img class="movies__img" src="https://image.tmdb.org/t/p/w500${properties.backdrop_path}" alt="" />
         <h3 class="movies__title"">${properties.original_title}</h3>
         <p class="movies__text">${properties.release_date}</p>
         <p class="movies__text">${properties.genre_ids}</p>
@@ -41,7 +42,7 @@ function clearMarcup() {
   getRef('.movies__list').innerHTML = ' ';
 }
 function clearMarcupModal() {
-  getRef('.modal-wrap').innerHTML = ' ';
+  getRef('.modal-wrap').remove();
 }
 async function getMoviesOnSearch(event) {
   event.preventDefault();
@@ -60,6 +61,7 @@ function toggleModal() {
 function closeModal() {
   clearMarcupModal();
   toggleModal();
+  window.removeEventListener('keydown', onEscClose);
 }
 function createMurkupModal({
   backdrop_path,
@@ -73,8 +75,9 @@ function createMurkupModal({
   const genresEl = genres.map(el => {
     return el.name;
   });
+  window.addEventListener('keydown', onEscClose);
   return `<div class="modal-wrap"><div class="modal-movie">
-  <img class="modal-movie__img" src="${backdrop_path}" alt="" />
+  <img class="modal-movie__img" src="https://image.tmdb.org/t/p/w500${backdrop_path}" alt="" />
   <h2 class="modal-movie__title">${original_title}</h2>
   <p class="modal-movie__text">Vote / Votes<span class="modal-movie__desc"> ${vote_average} / ${vote_count}</span></p>
   <p class="modal-movie__text">Popularity<span class="modal-movie__desc"> ${popularity}</span></p>
@@ -98,7 +101,21 @@ async function renderMarkupModal(e) {
   const markup = createMurkupModal(getAxios.data);
   getRef('.modal').insertAdjacentHTML('beforeend', markup);
 }
+function onEscClose(event) {
+  if (event.key === 'Escape') {
+    closeModal();
+  }
+}
+function onClickClose(e) {
+  const targetValue = e.target.classList.value;
+  const classBackdrop = 'backdrop';
+  if (targetValue === classBackdrop) {
+    closeModal();
+  }
+}
+getRef('.backdrop').addEventListener('click', onClickClose);
 
 getRef('.search').addEventListener('submit', getMoviesOnSearch);
 
 getMovies();
+console.log(firebase);
