d90032840c94b11455743190ed46d46221151fb1
Inna <isko.inna.94@gmail.com>
2022-12-07 12:50:27 +0200

    1

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ const getRef = selector => document.querySelector(selector);
 const apiKey = '54c00021c1f0a4ca812033181f98909b';
 
 let movieID = '';
-// let currentPage = 1;
-// let quantityPage = 20;
 
 async function getMovies() {
   const getAxios = await axios.get(
@@ -76,18 +74,56 @@ function createMurkupModal({
   });
   window.addEventListener('keydown', onEscClose);
   return `<div class="modal-movie">
- <img class="modal-movie__img" src="https://image.tmdb.org/t/p/w500${backdrop_path}" alt="" /> 
-  <div class="modal-movie__info"> 
- <h2 class="modal-movie__title">${original_title}</h2>
-  <p class="modal-movie__text">Vote / Votes<span class="modal-movie__desc"> ${vote_average} / ${vote_count}</span></p>
-  <p class="modal-movie__text">Popularity<span class="modal-movie__desc"> ${popularity}</span></p>
-  <p class="modal-movie__text">Original Title<span class="modal-movie__desc"> ${original_title}</span></p>
-  <p class="modal-movie__text">Genre<span class="modal-movie__desc"> ${genresEl}</span></p>
-  <h3 class="modal-movie__caption">About </h3>
-  <p class="modal-movie__lead"> ${overview}</p>
+   <div class="modal-img"><img class="modal-movie__img" src="https://image.tmdb.org/t/p/w500${backdrop_path}" alt="" />
+   <button class="btn-play">Play</button></div>
+    <div class="modal-movie__info">
+   <h2 class="modal-movie__title">${original_title}</h2>
+    <p class="modal-movie__text">Vote / Votes<span class="modal-movie__desc"> ${vote_average} / ${vote_count}</span></p>
+    <p class="modal-movie__text">Popularity<span class="modal-movie__desc"> ${popularity}</span></p>
+    <p class="modal-movie__text">Original Title<span class="modal-movie__desc"> ${original_title}</span></p>
+    <p class="modal-movie__text">Genre<span class="modal-movie__desc"> ${genresEl}</span></p>
+    <h3 class="modal-movie__caption">About </h3>
+    <p class="modal-movie__lead"> ${overview}</p>
+    </div>
   </div>
-</div>
-`;
+  `;
+  // return `
+  //       <<img class="modal__img" src="https://image.tmdb.org/t/p/w500${backdrop_path}" alt="Обкладинка фільму">
+  //     <div class="modal-container-columns">
+  //       <p class="modal__name">${original_title}</p>
+  //       <div class="modal-conteiner-ul">
+  //         <ul class="modal__list-theme">
+  //         <li class="modal__item">
+  //           <p class="modal__item-text">Vote / Votes</p>
+  //         </li>
+  //         <li class="modal__item">
+  //           <p class="modal__item-text">Popularity</p>
+  //         </li>
+  //         <li class="modal__item">
+  //           <p class="modal__item-text">Original Title</p>
+  //         </li>
+  //         <li class="modal__item">
+  //           <p class="modal__item-text">Genre</p>
+  //         </li>
+  //       </ul>
+  //         <ul class="modal__list-value">
+  //         <li class="modal__item">
+  //           <p class="modal__item-value slash"><span class="modal__hightlight selected">${vote_average}</span>/<span class="modal__hightlight not-selected">${vote_count}</span></p>
+  //         </li>
+  //         <li class="modal__item">
+  //           <p class="modal__item-value">${popularity}</p>
+  //         </li>
+  //         <li class="modal__item">
+  //           <p class="modal__item-value">${original_title}</p>
+  //         </li>
+  //         <li class="modal__item">
+  //           <p class="modal__item-value">${genresEl}</p>
+  //         </li>
+  //         </ul>
+  //       </div>
+  //       <p class="modal__about">About</p>
+  //       <p class="modal__description">${overview}</p>
+  // `;
 }
 async function renderMarkupModal(e) {
   toggleModal();
@@ -100,7 +136,8 @@ async function renderMarkupModal(e) {
 
   const markup = createMurkupModal(getAxios.data);
   getRef('.modal-wrap').insertAdjacentHTML('beforeend', markup);
-  await getRef('.modal-movie__btn').addEventListener('click', getTrailer);
+  await getRef('.btn-play').addEventListener('click', getTrailer);
+  // getRef('.modal-movie__img').addEventListener('mouseover', btnTrailer);
 }
 function onEscClose(event) {
   if (event.key === 'Escape') {
@@ -120,18 +157,18 @@ getRef('.search').addEventListener('submit', getMoviesOnSearch);
 
 getMovies();
 
-function getTrailer() {
+async function getTrailer() {
   let idYoutub = '';
   const getAxios = axios.get(
     `https://api.themoviedb.org/3/movie/${movieID}/videos?api_key=${apiKey}&language=en-US`
   );
-  getAxios.then(resp => {
+  await getAxios.then(resp => {
     idYoutub = resp.data.results[0].key;
     console.log(idYoutub);
   });
 
   const instance = basicLightbox.create(`
-    <iframe class="trailerPlayer" src="https://www.youtube.com/embed/${idYoutub}" width="1200" height="800" frameborder="0"></iframe>
+    <iframe class="trailerPlayer" src="https://www.youtube.com/embed/${idYoutub}" width="500" height="300" frameborder="0"></iframe>
 `);
 
   instance.show();
